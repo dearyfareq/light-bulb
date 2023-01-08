1f1921a0216d33fea5b067aa478fd8720004df1f
Diyari <dearyfareq15@gmail.com>
2023-01-08 22:37:23 +0300

    added buttons and js functions

---
+++ js.js
@@ -0,0 +1,8 @@
+function changeoff(){
+    var img = document.getElementById('bulb');
+    img.setAttribute('src','/light-bulb/light-bulb off.png'); 
+}
+function changeon(){
+    var img = document.getElementById('bulb');
+    img.setAttribute('src','/light-bulb/light-bulb on.png'); 
+}
