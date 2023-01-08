a98015b79d058b66c257dec1d4304bde4c8dde47
Diyari <dearyfareq15@gmail.com>
2023-01-08 22:39:50 +0300

    added audio file for clicking

--- js.js
+++ js.js
@@ -1,8 +1,10 @@
 function changeoff(){
     var img = document.getElementById('bulb');
     img.setAttribute('src','/light-bulb/light-bulb off.png'); 
+    document.getElementById("myAudio").play();
 }
 function changeon(){
     var img = document.getElementById('bulb');
     img.setAttribute('src','/light-bulb/light-bulb on.png'); 
+    document.getElementById("myAudio").play();
 }
